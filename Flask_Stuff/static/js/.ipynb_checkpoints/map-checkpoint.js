a49b66822bca422c0eab8fa163298d2f57bb05fc
Evan Prosser <evan.prosser@outlook.com>
2022-03-27 15:23:29 -0600

    Changed basemap

--- Flask_Stuff/static/js/.ipynb_checkpoints/map-checkpoint.js
+++ Flask_Stuff/static/js/.ipynb_checkpoints/map-checkpoint.js
@@ -1,11 +1,14 @@
 // Initialize the map
-var this_map = L.map('map').setView([51.0447, -114.0719], 11);
+var this_map = L.map('map').setView([50.64146,-113.64648], 13);
 
 // Add baselayer
-    L.tileLayer('http://mts3.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',{
-        maxZoom:22,
-        maxNativeZoom:18
-    }).addTo(this_map);
+L.tileLayer('https://api.mapbox.com/styles/v1/eprosser88/cl19riul2003215pcc5nlw7b9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXByb3NzZXI4OCIsImEiOiJja3p5d3FoNm4wNGo1M2tuZmNnaHphc2cxIn0.gguHDZWaDus2L4AW0h97rA', {
+    id: 'satellitestreets',
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+	attribution: '©  <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="https://www.openstreetmap.org/about/">OpenStreeMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a </strong> © <a href="https://www.maxar.com/">Maxar'
+}).addTo(this_map);
 
 // Add scale
 L.control.scale().addTo(this_map);
