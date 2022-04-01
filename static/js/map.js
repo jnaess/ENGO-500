3c0c54adb19ebb4fbb5e7c528b566630c108fb92
jnaess <Jan Erik Naess>
2022-03-31 21:01:10 -0600

    map updated for correct centering

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,6 @@
 // Initialize the map
-var this_map = L.map('map').setView([50.64146,-113.64648], 13);
+//var this_map = L.map('map').setView([50.64146,-113.64648], 13);
+var this_map = L.map('map').setView([40.202162,-74.924594], 13);
 
 // Add baselayer
 L.tileLayer('https://api.mapbox.com/styles/v1/eprosser88/cl19riul2003215pcc5nlw7b9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXByb3NzZXI4OCIsImEiOiJja3p5d3FoNm4wNGo1M2tuZmNnaHphc2cxIn0.gguHDZWaDus2L4AW0h97rA', {
