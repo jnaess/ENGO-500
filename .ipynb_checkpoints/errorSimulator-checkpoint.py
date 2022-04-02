8294bf02c996b29c4ab4b4e72c85043e604f46fd
jnaess <Jan Erik Naess>
2022-04-02 14:36:09 -0600

    Initial manager created

---
+++ .ipynb_checkpoints/errorSimulator-checkpoint.py
@@ -0,0 +1,198 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math as m
+from matplotlib import collections  as mc
+plt.style.use('seaborn-whitegrid')
+import pylab as pl
+
+from polygon import polygon
+from point import Coord
+from geomTools import GeomTools
+from positionGenerator import PositionGenerator
+from errorDocumentor import ErrorDocumentor
+
+class ErrorSimulator(ErrorDocumentor):
+    """
+    Simulates the error which is incorporated in pathFollower
+    """
+    
+    def __init__(self, drift_on = False, jump_on = False, 
+                 easting_drift_const = .01,
+                northing_drift_const = .01):
+        """
+        Desc:
+        Input:
+        Output:
+            self.errors = [Coord(), ... , Coord()] errors within each interval (drift and jump added together)
+            self.drift_errors = [Coord(), ... , Coord()]
+            self.jump_errors = [Coord(), ... , Coord()]
+            self.total_error, Coord() of the cumulative error
+            self.is_real = False, whether or not this is a "Real and error filled" or "True point" --> which does not record errors because they're always zero
+            self.drift_on = False, whether or not to generate a drift error
+            self.jump_on = False, whether or not to generate a jump error
+            self.easting_drift_const
+            self.northing_drift_const
+        """
+        ErrorDocumentor.__init__(self)
+        
+        self.jump_happened = False
+        self.is_real = False
+        self.drift_on = drift_on
+        self.jump_on = jump_on
+        
+        
+        self.easting_drift_const = easting_drift_const #average drift per meter
+        self.northing_drift_const = northing_drift_const #average drift per meter
+        
+        self.total_error = Coord(0,0)
+        self.errors = []
+        self.drift_errors = []
+        self.jump_errors = []
+        
+        self.pg = PositionGenerator()
+
+    def update_PG(self, pg):
+        """
+        Desc:
+            Updated the error simulator's PositionGenerator for the next add_error
+        Input:
+            pg, PositionGenerator() for the drift error in error per meter travelled by the tractor
+        Output:
+        """
+        self.pg=pg
+        
+    def add_error(self, interval, start = False):
+        """
+        Desc:
+            returns the error to be added either E, N, or H | default value is 0
+        Input:
+            interval, float --> the distance that the error is being multiplied by (calculated to be 1m --> then scaled by interval distance)
+            pg, PositionGenerator() for the drift error in error per meter travelled by the tractor
+        Output:
+            return --> float | default 0
+        """   
+        if start:
+            #then it is real and needs an error to be added
+            
+            self.drift_e = 0
+            self.drift_n = 0
+            
+            self.jump_e = 0
+            self.jump_n = 0
+
+            #record total epoch error
+            self.errors.append(Coord(self.drift_e+self.jump_e,self.drift_n+self.jump_n))
+
+            self.total_error.e = self.total_error.E()+self.drift_e+self.jump_e
+            self.total_error.n = self.total_error.N()+self.drift_n+self.jump_n
+            
+            self.record_errors()
+            
+        elif self.is_real:
+            """
+            TODO:
+                Cut increments if the next point excedes the outerfield
+            """
+            #then it is real and needs an error to be added
+            
+            #generate drift error
+            self.add_drift_error(interval)
+            
+            #generate jump error
+            self.add_jump_error(interval)
+
+            #record total epoch error
+            self.errors.append(Coord(self.drift_e+self.jump_e,self.drift_n+self.jump_n))
+
+            self.total_error.e = self.total_error.E()+self.drift_e+self.jump_e
+            self.total_error.n = self.total_error.N()+self.drift_n+self.jump_n
+            
+            self.record_errors()
+            
+    def add_drift_error(self, interval):
+        """
+        Desc:
+            Generated self.e and self.n drift error in relative to interval length
+        Input:
+            interval, float --> the distance that the error is being multiplied by (calculated to be 1m --> then scaled by interval distance)
+            pg, PositionGenerator() for the drift error in error per meter travelled by the tractor
+        Output:
+            self.drift_e --> current easting drift error
+            self.drift_n --> current northing drift error
+        """
+        if self.drift_on:
+            #generate new statistical random error
+            self.pg.generate_one()
+
+            #generate drift error
+            self.drift_e = interval*self.easting_drift_const+self.pg.unique_pnt.E()
+            self.drift_n = interval*self.northing_drift_const+self.pg.unique_pnt.N()
+            
+        else:
+            #then the error for this is zero
+            #generate drift error
+            self.drift_e = 0
+            self.drift_n = 0
+            
+        #record drift error
+        self.drift_errors.append(Coord(self.drift_e,self.drift_n))
+        
+    def add_jump_error(self, interval):
+        """
+        Desc:
+            ***INCOMPLETE**
+            Generated self.e and self.n jump error in relative to interval length
+        Input:
+            interval, float --> the distance that the error is being multiplied by (calculated to be 1m --> then scaled by interval distance) **maybe?? for jupm error...**
+            pg, PositionGenerator() for the drift error in error per meter travelled by the tractor
+        Output:
+            self.jump_e --> current easting drift error
+            self.jump_n --> current northing drift error
+        """
+        if self.jump_on:
+            #set to zero because probability is not get included
+            self.jump_e = 0
+            self.jump_n = 0
+            
+            if abs(self.jump_e) > 0 or abs(self.jump_n) > 0:
+                self.jump_happened = True
+            else:
+                self.jump_happened = False
+        else:
+            #then the error for this is zero
+            #generate jump error
+            self.jump_e = 0
+            self.jump_n = 0
+        
+        #record jump error
+        self.jump_errors.append(Coord(self.jump_e,self.jump_n))
+        
+    def E(self):
+        """
+        Desc:
+            if self.is_real = True --> returns the most previous easting error
+            otherwise returns 0 because it is a true point that doesn't have an error
+        Input:
+        Output:
+            returns --> Easting error --> float
+        """
+        if self.is_real:
+            #then has an error and therefore the error has been generated
+            return self.errors[-1].E()
+        else:
+            return 0
+        
+    def N(self):
+        """
+        Desc:
+            if self.is_real = True --> returns the most previous northing error
+            otherwise returns 0 because it is a true point that doesn't have an error
+        Input:
+        Output:
+            returns --> northing error --> float
+        """
+        if self.is_real:
+            #then has an error and therefore the error has been generated
+            return self.errors[-1].N()
+        else:
+            return 0
---
+++ .ipynb_checkpoints/manager-checkpoint.py
@@ -0,0 +1,116 @@
+import psycopg2
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+
+from simulator import Simulator
+from errorDetector import ErrorDetector
+from databaseManager import DatabaseManager
+
+class Manager(DatabaseManager):
+    """
+    Contains all simulation and error detection software to execute the primary processes in one centralized location
+    """
+    
+    def __init__(self):
+        """
+        Desc:
+        Input:
+        Output
+        """
+        DatabaseManager.__init__(self)
+        
+        self.retrieve_sim_data()
+        self.initialize_simulator()
+        self.initialize_error_detector()
+        self.push_data()
+    
+    def retrieve_sim_data(self, sim_id = -1):
+        """
+        Desc:
+            Retieves data from the 'simulations' table for the specified simulation
+        Input:
+        Output:
+        """
+        con = self.engine.connect()
+
+        self.sim_df= pd.DataFrame(self.select_all(con=con, table = 'simulations'))
+        
+        if sim_id == -1:
+            #set simulation number for reference
+            self.sim_id = self.sim_df.id.max()
+        else:
+            self.sim_id = sim_id
+            
+        con.close()
+        
+    def initialize_simulator(self):
+        """
+        Desc:
+            sets up the simulator that will be used
+        Input:
+            sim_id
+        Output:
+            self.sim
+        """
+        #simulation id for accessing data from database
+        sim_id = 2
+
+        #coordinates
+        self.field = [[0,0],[0,10],[10,10],[10,0]]
+
+        #generates simulation tracks
+        self.Sim = Simulator(self.field, use_drift=True, easting_drift_const = .01, northing_drift_const = .01)
+
+        #generates dataframe of values
+        self.Sim.generate_output_tracks()
+        
+        #stores dataframe of values in a variable
+        self.df_sim = self.Sim.output_tracks
+        
+    def initialize_error_detector(self):
+        """
+        Desc:
+            sets up the error detector
+        Input:
+        Output:
+        """
+        #convert index to epochs
+        self.df_sim["epoch"] = list(self.df_sim.index.values)
+
+        #get key values as lists to be passed in
+        std = [self.df_sim['true_e_std'].to_list(),self.df_sim['true_n_std'].to_list()]
+        tru_N = self.df_sim['true_n'].to_list()
+        tru_E = self.df_sim['true_e'].to_list()
+        rename_keys = ["epoch", "real_e", "real_n", "real_e_std", "real_n_std"]
+
+        #run error detector simulation
+        self.ED = ErrorDetector(self.df_sim, tru_E, tru_N, rename_keys = rename_keys, true_std = std, is_static=False)
+
+        #store error detector values as a dataframe
+        self.ED.generate_error_dataframe()
+
+        #store error_detector values
+        self.df_err_jump = self.ED.jump_df
+        self.df_err_drift = self.ED.drift_df
+        self.df_err_errors = self.ED.errors_df
+        
+    def push_data(self):
+        """
+        Desc:
+            pushes the set up datframes into the database
+        Input:
+        Output:
+        """
+        con = self.engine.connect()
+
+        self.df_err_jump["sim_id"] = np.ones(len(self.df_err_jump.index))*self.sim_id
+        self.df_err_jump["epoch"] = list(self.df_err_jump.index.values)
+
+        self.df_err_jump["jump_status"] = self.df_err_jump["jump_status"].astype('bool')
+
+        self.df_err_jump.to_sql('error_detector_jumps', con=con, if_exists='append', index=False)
+        
+        con.close()
+        
+        
